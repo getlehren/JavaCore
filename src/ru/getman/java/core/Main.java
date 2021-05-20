ee7389b7f26e72b5d825e288453c187b3e362c70
Getman <lehren2048@gmail.com>
2021-05-20 15:48:02 +0200

    HW-2 Getman

--- src/ru/getman/java/core/Main.java
+++ src/ru/getman/java/core/Main.java
@@ -3,6 +3,20 @@ package ru.getman.java.core;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[][] array =
+                {{"1","mew","1","1"},
+                {"1","1","qwe","1"},
+                {"1","mnm","1","1"},
+              //  {"1","t","1","1"}, //для проверки массива 4х4
+                {"1","asd","1","fff"}};
+        Lesson2 homework = new Lesson2();
+
+
+        try {
+            System.out.println(homework.sumArrayElements(array));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        }
     }
 }
