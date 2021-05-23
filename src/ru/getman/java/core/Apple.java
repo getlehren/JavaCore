963e017edb20a676ef759274b2620d14db71d882
Getman <lehren2048@gmail.com>
2021-05-23 04:33:24 +0200

    HW-3 Getman. Сложно реализовывать ArrayList, когда мы его не проходили.

---
+++ src/ru/getman/java/core/Apple.java
@@ -0,0 +1,11 @@
+package ru.getman.java.core;
+
+public class Apple extends Fruit {
+
+    public float weight = 1.0f;
+
+    @Override
+    float getWeight() {
+        return this.weight;
+    }
+}
